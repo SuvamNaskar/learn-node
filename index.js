f076bc7cf9affd321a5c3403226112097ca5d6bf
Suvam Naskar <suvamnaskar780@gmail.com>
2025-09-03 09:10:32 +0530

    refactor: reorganize routes and middleware setup in index.js

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express');
-const userRoutes = require('./routes/users');
+const mongoose = require('mongoose');
+const homeRoutes = require('./routes/index');
+const taskApiRoutes = require('./routes/task-api');
+const methodOverride = require('method-override');
+
+const connectDB = require('./Connection');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-app.use('/users', userRoutes);
+app.use(express.urlencoded({ extended: true })); // For form data
+app.use(express.json());
+app.use(methodOverride('_method')); // For PUT/DELETE via forms
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use('/', homeRoutes);
+app.use('/api', taskApiRoutes);
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
